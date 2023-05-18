ffb98ba1bce5f88f840c539890ce8091aaff9d6c
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-05-19 00:20:59 +0700

    add closest prime

---
+++ src/Lecture04_Function/ClosestPrime.java
@@ -0,0 +1,41 @@
+package Lecture04_Function;
+
+import java.util.Scanner;
+
+public class ClosestPrime {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int closestPrime = 0;
+        for (int m = n-1; m >= 2; m--) {
+            if (isPrime(m)) {
+                closestPrime = m;
+                break;
+            }
+        }
+        for (int m = n+1; m <= 10000; m++) {
+            if (isPrime(m)) {
+                if (closestPrime == 0 || Math.abs(m-n) < Math.abs(closestPrime-n)) {
+                    closestPrime = m;
+                } else if (Math.abs(m-n) == Math.abs(closestPrime-n) && m < closestPrime) {
+                    closestPrime = m;
+                }
+                break;
+            }
+        }
+        System.out.println(closestPrime);
+        sc.close();
+    }
+}
