d540cc6c7972c17e5b7b6fcea659ffa365c177e3
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-05-27 02:22:22 +0700

    fix closest prime

--- src/Lecture04_Function/ClosestPrime.java
+++ src/Lecture04_Function/ClosestPrime.java
@@ -7,38 +7,36 @@ public class ClosestPrime {
         if (n <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n)
-                ; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int closestPrime = 0;
-        for (int m = n; m <= 10000; m++) {
-            if (isPrime(m)) {
-                closestPrime = m;
-                break;
+        if (isPrime(n)) {
+            System.out.print(n);
+        } else {
+            int m = n + 1;
+            while (!isPrime(m)) {
+                m++;
+            }
+            int p = n - 1;
+            while (!isPrime(p)) {
+                p--;
+            }
+            if (n - p < m - n) {
+                System.out.print(p);
+            } else if (n - p == m - n) {
+                System.out.print(p);
+            } else {
+                System.out.print(m);
             }
         }
-        if (closestPrime == 0) {
-            for (int m = n-1; m >= 2; m--) {
-                if (isPrime(m)) {
-                    closestPrime = m;
-                    break;
-                }
-                else if (Math.abs(m-n) < Math.abs(closestPrime-n)) {
-                    closestPrime = m;
-                } else if (Math.abs(m-n) == Math.abs(closestPrime-n) && m < closestPrime) {
-                    closestPrime = m;
-                }
-            }
-        }
-        System.out.println(closestPrime);
-        sc.close();
+
     }
 }
