3bd6e8d402b499597078261529e61997a8ad1c63
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-01 22:24:17 +0700

    add sum line

---
+++ src/Lecture06_2DArray/SumLine.java
@@ -0,0 +1,28 @@
+package Lecture06_2DArray;
+
+import java.util.Scanner;
+
+public class SumLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += matrix[i][j];
+            }
+            System.out.println(i + ": " + sum);
+        }
+    }
+}
