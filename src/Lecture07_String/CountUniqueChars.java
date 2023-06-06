c75c7193e326ad607fd01b607c28d2cb1cf62997
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-07 00:47:18 +0700

    fix count unique chars

--- src/Lecture07_String/CountUniqueChars.java
+++ src/Lecture07_String/CountUniqueChars.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 
 public class CountUniqueChars {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String s = input.nextLine();
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
         int[] charCounts = new int[128];
 
         for (char c : s.toCharArray()) {
             charCounts[c]++;
         }
-
         int numUniqueChars = 0;
-
         for (int count : charCounts) {
             if (count > 0) {
                 numUniqueChars++;
             }
         }
-
         System.out.println(numUniqueChars);
+        sc.close();
     }
 }
