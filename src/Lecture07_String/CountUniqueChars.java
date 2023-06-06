3eda282c2118aa1a42a8959b975e6ff9cec5f89d
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-07 00:43:44 +0700

    add count unique chars

---
+++ src/Lecture07_String/CountUniqueChars.java
@@ -0,0 +1,26 @@
+package Lecture07_String;
+
+import java.util.Scanner;
+
+public class CountUniqueChars {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+
+        int[] charCounts = new int[128];
+
+        for (char c : s.toCharArray()) {
+            charCounts[c]++;
+        }
+
+        int numUniqueChars = 0;
+
+        for (int count : charCounts) {
+            if (count > 0) {
+                numUniqueChars++;
+            }
+        }
+
+        System.out.println(numUniqueChars);
+    }
+}
