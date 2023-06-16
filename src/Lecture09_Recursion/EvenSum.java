6ce468fa8a081f81a3fb88a4a4ed109201aafb1a
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-17 00:21:40 +0700

    add sum of even

---
+++ src/Lecture09_Recursion/EvenSum.java
@@ -0,0 +1,28 @@
+package Lecture09_Recursion;
+
+import java.util.Scanner;
+
+public class EvenSum {
+    public static int sum(int[] arr, int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            int s = sum(arr, n-1);
+            if (arr[n-1] % 2 == 0) {
+                s += arr[n-1];
+            }
+            return s;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(sum(arr, n));
+        sc.close();
+    }
+}
