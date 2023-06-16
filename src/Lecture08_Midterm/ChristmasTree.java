ee884305fc59b647537f7c07723ff61637d9f564
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-16 21:24:29 +0700

    add sudoku

--- src/Lecture08_Midterm/ChristmasTree.java
+++ src/Lecture08_Midterm/ChristmasTree.java
@@ -6,14 +6,18 @@ public class ChristmasTree
 {
     public static void main(String[] args)
     {
+        // n = 3
+        //   *
+        //  * *
+        // * * *
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
         int width = n * 2 - 1;
         char[][] tree = new char[n][width];
-        int nSpace = 0;
+        int nSpace = 0; // so khoang trang phai in tai moi dong
         for (int i = n - 1; i >= 0; --i)
         {
-            char[] row = new char[width];
+            char[] row = new char[width]; // ki tu tai moi dong
             int ind = 0;
             for (int space = 0; space < nSpace / 2; ++space)
             {
