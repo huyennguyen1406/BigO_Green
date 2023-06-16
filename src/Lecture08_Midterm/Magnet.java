7dc536d1458e5214491e7c776e816dd1cf07fe05
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-16 21:25:17 +0700

    add magnet

---
+++ src/Lecture08_Midterm/Magnet.java
@@ -0,0 +1,22 @@
+package Lecture08_Midterm;
+
+import java.util.Scanner;
+
+public class Magnet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        String prev = sc.next();
+
+        for (int i = 1; i < n; i++) {
+            String curr = sc.next();
+            if (!curr.equals(prev)) {
+                count++;
+                prev = curr;
+            }
+        }
+        System.out.println(count);
+    }
+}
+}
