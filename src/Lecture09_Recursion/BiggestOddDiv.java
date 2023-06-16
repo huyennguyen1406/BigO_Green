67462ba8f4a79ce06a6282e3956f8fd3f40e7eca
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-16 21:27:45 +0700

    add biggest odd div

---
+++ src/Lecture09_Recursion/BiggestOddDiv.java
@@ -0,0 +1,20 @@
+package Lecture09_Recursion;
+
+import java.util.Scanner;
+
+public class BiggestOddDiv {
+    public static int largestOddDivisor(int n) {
+        if (n % 2 == 0) {
+            return largestOddDivisor(n/2);
+        } else {
+            return n;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(largestOddDivisor(n));
+    }
+}
+}
