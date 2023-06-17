96aa15aab17b4c2fbf14469959e8e164310fac90
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-06-18 01:20:15 +0700

    add greatest common div

---
+++ src/Lecture09_Recursion/GreatestDivisor.java
@@ -0,0 +1,23 @@
+package Lecture09_Recursion;
+
+import java.util.Scanner;
+
+public class GreatestDivisor {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(gcd(a, b));
+        sc.close();
+    }
+}
