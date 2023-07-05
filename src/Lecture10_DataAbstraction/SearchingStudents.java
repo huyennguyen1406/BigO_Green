edf746386e191c61ad566258fb2c8b986a92aeaa
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-07-06 00:53:52 +0700

    fix searching students

--- src/Lecture10_DataAbstraction/SearchingStudents.java
+++ src/Lecture10_DataAbstraction/SearchingStudents.java
@@ -17,14 +17,14 @@ class Testing {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), q = sc.nextInt();
         SearchingStudents []students = new SearchingStudents[n];
-        for(int i=0; i<n; ++i){
+        for(int i = 0; i < n; ++i){
             String id = sc.next(), math = sc.next(), lit = sc.next();
             students[i] = new SearchingStudents(id, math, lit);
         }
         String id;
-        for(int i=0; i<q; ++i){
+        for(int i = 0; i < q; ++i){
             id = sc.next();
-            for(int j=0; j<n; ++j){
+            for(int j = 0; j < n; ++j){
                 if(students[j].id.compareTo(id) == 0){
                     System.out.printf("%s %s\n", students[j].math, students[j].lit);
                     break;
