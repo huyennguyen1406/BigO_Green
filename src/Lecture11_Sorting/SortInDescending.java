a7671e2efe6d80813c032a182adc288db0f6abd7
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-07-11 01:16:19 +0700

    add sort in descending order 2

--- src/Lecture11_Sorting/SortInDescending.java
+++ src/Lecture11_Sorting/SortInDescending.java
@@ -23,10 +23,10 @@ public class SortInDescending {
             int key = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] < key) {
-                arr[j+1] = arr[j];
+                arr[j + 1] = arr[j];
                 j = j - 1;
             }
-            arr[j+1] = key;
+            arr[j + 1] = key;
         }
     }
 }
