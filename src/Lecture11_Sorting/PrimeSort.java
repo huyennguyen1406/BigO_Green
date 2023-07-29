1a3f87a835193d57218e4816da5eb7a80cadb824
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-07-30 01:09:48 +0700

    fix prime sort

--- src/Lecture11_Sorting/PrimeSort.java
+++ src/Lecture11_Sorting/PrimeSort.java
@@ -1,60 +1,54 @@
 package Lecture11_Sorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrimeSort {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int[] arr = new int[n];
-        boolean[] isPrime = new boolean[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            isPrime[i] = isPrimeNumber(arr[i]);
+            arr[i] = scanner.nextInt();
         }
-        insertionSort(arr);
+        sortArray(arr);
         for (int i = 0; i < n; i++) {
-            if (isPrime[i]) {
-                int index = findIndex(arr[i], arr);
-                arr[index] = arr[i];
-                arr[i] = -1; // đánh dấu phần tử đã được sắp xếp
-            }
             System.out.print(arr[i] + " ");
         }
-        sc.close();
+        scanner.close();
     }
-
-    public static boolean isPrimeNumber(int n) {
-        if (n < 2) {
+    public static void sortArray(int[] arr) {
+        List<Integer> primes = new ArrayList<>();
+        List<Integer> nonPrimes = new ArrayList<>();
+        for (int num : arr) {
+            if (isPrimeNumber(num)) {
+                primes.add(num);
+            } else {
+                nonPrimes.add(num);
+            }
+        }
+        Collections.sort(nonPrimes);
+        int primeIndex = 0;
+        int nonPrimeIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (isPrimeNumber(arr[i])) {
+                arr[i] = primes.get(primeIndex++);
+            } else {
+                arr[i] = nonPrimes.get(nonPrimeIndex++);
+            }
+        }
+    }
+    public static boolean isPrimeNumber(int num) {
+        if (num <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
-    }
-
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
-
-    public static int findIndex(int num, int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
-                return i;
-            }
-        }
-        return -1;
     }
 }
